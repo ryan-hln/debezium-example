0fe5abb93bb6d6f581e690a2b1ea36092ad1e8a1
Ryan Rogers <ryan.rogers@hln.com>
2025-05-27 15:18:43 -0700

    Add a second Postgres server.
    Added a second Postgres server as a read-only replica.

--- docker/postgres-1/entrypoint/01-init.sh
+++ docker/postgres-1/entrypoint/01-init.sh
@@ -2,6 +2,7 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "${POSTGRES_USER}" --dbname "${POSTGRES_DB}" <<-EOSQL
+  SELECT pg_create_physical_replication_slot('repl_slot');
   CREATE USER debezium WITH ENCRYPTED PASSWORD '321muizebed' REPLICATION LOGIN;
   CREATE DATABASE debezium;
   GRANT ALL PRIVILEGES ON DATABASE debezium TO debezium;
